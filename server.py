48ad6d899ebddb14c32139b7efd304202ab3fbd8
AlejandroEsLo <alex-lojim@hotmail.com>
2018-11-19 19:55:13 +0100

    Añadidos el resto de metodos (Falta probar ACK)

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
+
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -27,31 +29,41 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                     
             mensaje_cliente = line.decode("utf-8").split(" ")
             metodo = mensaje_cliente[0]
-            
+            Ip = self.client_address[0]
+
             if metodo == "INVITE":
                 respuesta_serv = ("SIP/2.0 100 Trying\r\n\r\n" \
                                     + "SIP/2.0 180 Ringing\r\n\r\n" \
                                     + "SIP/2.0 200 OK\r\n\r\n")
-            
                 self.wfile.write(bytes(respuesta_serv, "utf-8"))
             
             elif metodo == "ACK":
-               print("LLEGA ACK")
+               # aEjecutar es un string con lo que ejcutara en la shell
+               aEjecutar = ("mp32rtp -i " + Ip + " -p 23032 < " + fich_audio)
+               print("Vamos a ejecutar", aEjecutar)
+               os.system(aEjecutar)
 
             elif metodo == "BYE":            
                 respuesta_serv = ("SIP/2.0 200 OK\r\n\r\n")
-            
                 self.wfile.write(bytes(respuesta_serv, "utf-8"))
             
+            elif metodo != ["INVITE","ACK","BYE"]:            
+                respuesta_serv = ("SIP/2.0 405 Method Not Allowed\r\n\r\n")
+                self.wfile.write(bytes(respuesta_serv, "utf-8"))
+                
+            else:
+                respuesta_serv = ("SIP/2.0 400 Bad Request\r\n\r\n")
+                self.wfile.write(bytes(respuesta_serv, "utf-8"))
+
 if __name__ == "__main__":
     try:
         Ip_serv = sys.argv[1]
         puerto_serv = int(sys.argv[2])
         fich_audio = sys.argv[3]
-    except :
+    except IndexError:
         sys.exit("Usage: python3 server.py IP port audio_file")
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', puerto_serv), EchoHandler)
+    serv = socketserver.UDPServer((Ip_serv, puerto_serv), EchoHandler)
     print("Listening...")    
     try:
         serv.serve_forever()
