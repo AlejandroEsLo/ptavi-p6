4b6c9bcb99bc2dd27c49ed8fd0531f59cd8c7600
AlejandroEsLo <alex-lojim@hotmail.com>
2018-11-19 19:54:23 +0100

    correccion excepciones

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ try:
     IP_RECEPTOR = IP_Y_PORT.split(":")[0] # 127.0.0.1
     PORT = int(sys.argv[2].split(":")[-1])
     
-except :
+except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
@@ -34,13 +34,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     data = my_socket.recv(1024)
     
     respuesta_serv = data.decode('utf-8')
+    #End para quitar espacio print
+    print('Recibido:\r\n', end = data.decode('utf-8'))
     
     if respuesta_serv == ("SIP/2.0 100 Trying\r\n\r\n" \
                             + "SIP/2.0 180 Ringing\r\n\r\n" \
                             + "SIP/2.0 200 OK\r\n\r\n"):
-        #End para quitar espacio print
-        print('Recibido:\r\n', end = data.decode('utf-8')) 
-        
         METODO = "ACK"
         mensaje = (METODO.upper() + " sip:" + LOGIN + "@" + IP_RECEPTOR \
                 + " SIP/2.0\r\n")
